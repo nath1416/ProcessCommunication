47c2475e82e5a02193df72b315ca76a9d5a3ab9d
Nath1416 <bougienath@gmail.com>
2024-12-23 16:39:01 -0500

    Ajout de condition variables

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <queue>
 #include <mutex>
+#include <condition_variable>
 
 std::mutex incomingMtx;
 std::mutex outcomingMtx;
@@ -10,6 +11,7 @@ std::mutex outcomingMtx;
 std::queue<std::string> incomingMessages;
 std::queue<std::string> outcomingMessages;
 
+std::condition_variable cv;
 
 void readFromStdin() {
     std::string line;
@@ -20,8 +22,9 @@ void readFromStdin() {
             incomingMessages.push(line);
             }
             {
-            std::lock_guard<std::mutex> lock(outcomingMtx);
+                std::lock_guard<std::mutex> lock(outcomingMtx);
                 outcomingMessages.push("received a packet");
+                cv.notify_one();
             }
             // std::cout << "SLAVE Read from stdin: " << line << std::endl;
 
@@ -34,15 +37,14 @@ void readFromStdin() {
 void writeToStdout() {
     int count = 0;
     while (true) {
-        while(!outcomingMessages.empty()){
-        {
-            std::lock_guard<std::mutex> lock(outcomingMtx);
-            std::string message = outcomingMessages.front();
-            outcomingMessages.pop();
-            std::cout << "SLAVE Write to stdout: " << message << std::endl;
-        }
-            // std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
-        }
+        std::unique_lock<std::mutex> lock(outcomingMtx);
+        
+        cv.wait(lock, []{return !outcomingMessages.empty();});
+
+        std::string message = outcomingMessages.front();
+        outcomingMessages.pop();
+        std::cout << "SLAVE Write to stdout: " << message << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
     }
 }
 
